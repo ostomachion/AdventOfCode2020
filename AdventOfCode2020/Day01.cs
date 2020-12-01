d2559d4e3cf0e36da97b853725c07751a6e67962
= <josh@hufford.io>
2020-12-01 16:37:27 -0500

    Implement day 1 part 1

--- AdventOfCode2020/Day01.cs
+++ AdventOfCode2020/Day01.cs
@@ -1,10 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AdventOfCode2020
 {
-    static class Day01
+    public static class Day01
     {
+        public static IEnumerable<int> ParseInput(string input) => input.Split('\n').Select(Int32.Parse);
+
         public static void Part1()
         {
-            
+            var report = ParseInput(Input.Get(1));
+            var (i, j) = FindTerms(report);
+            Console.WriteLine(i * j);
+        }
+
+        public static (int, int) FindTerms(IEnumerable<int> input)
+        {
+            int i = input.First(x => input.Contains(2020 - x));
+            return (i, 2020 - i);
         }
     }
 }
--- AdventOfCode2020Tests/Day01Test.cs
+++ AdventOfCode2020Tests/Day01Test.cs
@@ -1,8 +1,29 @@
+using System.Collections.Generic;
 using NUnit.Framework;
+using AdventOfCode2020;
 
 namespace AdventOfCode2020Tests
 {
-    public class Tests
+    public class Day01Tests
     {
+        [Test]
+        public void FindTermsTest()
+        {
+            // Given
+            var report = Day01.ParseInput(@"1721
+979
+366
+299
+675
+1456");
+
+            // When
+            var (i, j) = Day01.FindTerms(report);
+
+            // Then
+            Assert.AreEqual(1721, i);
+            Assert.AreEqual(299, j);
+            Assert.AreEqual(514579, i * j);
+        }
     }
 }
