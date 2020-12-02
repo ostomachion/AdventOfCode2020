ff3cb39c82f16d3056a3d4ec6a9596a236f91ba3
= <josh@hufford.io>
2020-12-01 22:41:20 -0500

    Add doc comments and argument check

--- AdventOfCode2020/Day01.cs
+++ AdventOfCode2020/Day01.cs
@@ -7,22 +7,43 @@ namespace AdventOfCode2020
 {
     public static class Day01
     {
+        /// <summary>
+        /// Converts a string containing a positive integer on each line to an <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
         public static IEnumerable<int> ParseInput(string input) => input.Split('\n').Select(Int32.Parse);
 
+        /// <summary>
+        /// https://adventofcode.com/2020/day/1
+        /// </summary>
         public static void Part1()
         {
             var report = ParseInput(Input.Get(1));
             Console.WriteLine(FindProduct(2, report));
         }
 
+        /// <summary>
+        /// https://adventofcode.com/2020/day/1#part2
+        /// </summary>
         public static void Part2()
         {
             var report = ParseInput(Input.Get(1));
             Console.WriteLine(FindProduct(3, report));
         }
 
+        /// <summary>
+        /// Returns the product of <paramref name="numberOfTerms"/> distinct values
+        /// from <paramref name="input"/> which sum to 2020.
+        /// </summary>
+        /// <param name="numberOfTerms">The number of terms to look for.</param>
+        /// <param name="input">The list of positive integers to choose from.</param>
+        /// <returns>The product of the terms which sum to 2020.</returns>
         public static BigInteger FindProduct(int numberOfTerms, IEnumerable<int> input)
         {
+            if (input.Any(x => x < 0))
+                throw new ArgumentException("All input terms must be positive.", nameof(input));
+
             return FindProduct(numberOfTerms, 2020, input.ToArray());
 
             static BigInteger FindProduct(int numberOfTerms, int goalSum, ReadOnlySpan<int> input)
