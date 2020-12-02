53d3105432f3c8a9818e8d99d1fcbeb895cc0cbc
= <josh@hufford.io>
2020-12-02 18:46:39 -0500

    Add doc comments to Input.cs

--- AdventOfCode2020/Input.cs
+++ AdventOfCode2020/Input.cs
@@ -2,10 +2,23 @@ using System.IO;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// A helper class for getting text from input files.
+    /// </summary>
     public static class Input
     {
+        /// <summary>
+        /// Returns the contents of an input file for a given day.
+        /// </summary>
+        /// <param name="day">The day to retrieve.</param>
+        /// <returns>The contents of the input file for the given day.</returns>
         public static string Get(int day) => File.ReadAllText(Path.Combine("Input", $"Day{day.ToString().PadLeft(2, '0')}.txt"));
 
+        /// <summary>
+        /// Returns the lines of an input file for a given day.
+        /// </summary>
+        /// <param name="day">The day to retrieve.</param>
+        /// <returns>The lines of the input file for the given day.</returns>
         public static string[] GetLines(int day) => Get(day).Split('\n');
     }
 }
