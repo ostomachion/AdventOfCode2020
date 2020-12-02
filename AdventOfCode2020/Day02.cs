68adc7b17ad1c21b1a2d8b9dffa191ab5ec4fcce
= <josh@hufford.io>
2020-12-02 07:32:58 -0500

    Setup for day 2

---
+++ AdventOfCode2020/Day02.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace AdventOfCode2020
+{
+    public static class Day02
+    {
+        /// <summary>
+        /// TODO:
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns></returns>
+        public static void ParseInput(string input) => throw new NotImplementedException();
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/2
+        /// </summary>
+        public static void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/2#part2
+        /// </summary>
+        public static void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2020Tests/Day02Tests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using AdventOfCode2020;
+using System.Linq;
+
+namespace AdventOfCode2020Tests
+{
+    public class Day02Tests
+    {
+        [Fact]
+        public void Part1Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Part2Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
