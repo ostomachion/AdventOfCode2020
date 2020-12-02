38e24ab282e18859188dec6eee4ea920ef2f76b6
= <josh@hufford.io>
2020-12-01 21:23:14 -0500

    More refactoring and optimization

--- AdventOfCode2020/Day01.cs
+++ AdventOfCode2020/Day01.cs
@@ -8,34 +8,46 @@ namespace AdventOfCode2020
     {
         public static IEnumerable<int> ParseInput(string input) => input.Split('\n').Select(Int32.Parse);
 
-        private static IEnumerable<int> report = ParseInput(Input.Get(1));
-
         public static void Part1()
         {
-            var result = FindTerms(2, 2020, report)!.ToArray();
-            Console.WriteLine(result[0] * result[1]);
+            var report = new ReadOnlySpan<int>(ParseInput(Input.Get(1)).ToArray());
+            Console.WriteLine(FindTerms(2, 2020, report, out _));
         }
 
         public static void Part2()
         {
-            var result = FindTerms(3, 2020, report)!.ToArray();
-            Console.WriteLine(result[0] * result[1] * result[2]);
+            var report = new ReadOnlySpan<int>(ParseInput(Input.Get(1)).ToArray());
+            Console.WriteLine(FindTerms(6, 2020, report, out _));
         }
 
-        public static IEnumerable<int>? FindTerms(int numberOfTerms, int goalSum, IEnumerable<int> input)
+        public static int? FindTerms(int numberOfTerms, int goalSum, ReadOnlySpan<int> input, out IEnumerable<int>? terms)
         {
             if (numberOfTerms == 0)
             {
-                return goalSum == 0 ? Enumerable.Empty<int>() : null;
+                if (goalSum == 0)
+                {
+                    terms = Enumerable.Empty<int>();
+                    return 1;
+                }
+                else
+                {
+                    terms = null;
+                    return null;
+                }
             }
 
-            foreach (var item in input)
+            for (var i = 0; i < input.Length; i++)
             {
-                var test = FindTerms(numberOfTerms - 1, goalSum - item, input.Except(new[] { item }));
-                if (test is not null)
-                    return new[] { item }.Concat(test);
+                var term = input[i];
+                var product = FindTerms(numberOfTerms - 1, goalSum - term, input[i..], out terms);
+                if (terms is not null)
+                {
+                    terms = new[] { term }.Concat(terms);
+                    return product * term;
+                }
             }
 
+            terms = null;
             return null;
         }
     }
--- AdventOfCode2020Tests/Day01Tests.cs
+++ AdventOfCode2020Tests/Day01Tests.cs
@@ -11,31 +11,33 @@ namespace AdventOfCode2020Tests
         public void Part1Test()
         {
             // Given
-            var report = Day01.ParseInput(Input.Get(1));
+            var report = new ReadOnlySpan<int>(Day01.ParseInput(Input.Get(1)).ToArray());
 
             // When
-            var terms = Day01.FindTerms(2, 2020, report);
+            var product = Day01.FindTerms(2, 2020, report, out var terms);
 
             // Then
             Assert.Collection(terms,
                 term => Assert.Equal(1721, term),
                 term => Assert.Equal(299, term));
+            Assert.Equal(514579, product);
         }
 
         [Fact]
         public void Part2Test()
         {
             // Given
-            var report = Day01.ParseInput(Input.Get(1));
+            var report = new ReadOnlySpan<int>(Day01.ParseInput(Input.Get(1)).ToArray());
 
             // When
-            var terms = Day01.FindTerms(3, 2020, report);
+            var product = Day01.FindTerms(3, 2020, report, out var terms);
 
             // Then
             Assert.Collection(terms,
                 term => Assert.Equal(979, term),
                 term => Assert.Equal(366, term),
                 term => Assert.Equal(675, term));
+            Assert.Equal(241861950, product);
         }
     }
 }
