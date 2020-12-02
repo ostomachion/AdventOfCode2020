9408023e2cde53275d28a22507aad15e6fcad9da
= <josh@hufford.io>
2020-12-02 18:38:10 -0500

    Add argument checks and doc comments

--- AdventOfCode2020/Classes/PasswordLine.cs
+++ AdventOfCode2020/Classes/PasswordLine.cs
@@ -4,21 +4,45 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// Represents a line from a password log, containing both the password
+    /// policy and the password.
+    /// </summary>
     public class PasswordLine
     {
         private static readonly Regex regex = new Regex(@"(?<min>\d+)-(?<max>\d+) (?<c>[a-z]): (?<pw>[a-z]+)");
 
+        /// <summary>
+        /// The password policy used to set the password.
+        /// </summary>
         public PasswordPolicy Policy { get; }
+
+        /// <summary>
+        /// The password. It may not satisfy the policy.
+        /// </summary>
         public string Password { get; }
 
+        /// <summary>
+        /// Creates an instance of <see cref="PasswordLine"/>.
+        /// </summary>
+        /// <param name="policy">The <see cref="PasswordLine"/>.</param>
+        /// <param name="password">The password.</param>
         public PasswordLine(PasswordPolicy policy, string password)
         {
-            Policy = policy;
-            Password = password;
+            Policy = policy ?? throw new ArgumentNullException(nameof(policy));
+            Password = password ?? throw new ArgumentNullException(nameof(password));
         }
 
+        /// <summary>
+        /// Creates an instance of <see cref="PasswordLine"/> from a line in a password file.
+        /// </summary>
+        /// <param name="line">The line from a password file.</param>
+        /// <returns>The parsed <see cref="PasswordLine"/></returns>
         public static PasswordLine Parse(string line)
         {
+            if (line is null)
+                throw new ArgumentNullException(nameof(line));
+
             var match = regex.Match(line);
             if (match is null)
                 throw new ArgumentException(nameof(line));
--- AdventOfCode2020/Classes/PasswordPolicy.cs
+++ AdventOfCode2020/Classes/PasswordPolicy.cs
@@ -3,12 +3,38 @@ using System.Linq;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// Represents a password policy requiring either a
+    /// certain number of a specific character, or a
+    /// character in a specific location.
+    /// </summary>
     public class PasswordPolicy
     {
+        /// <summary>
+        /// The character required for the password.
+        /// </summary>
         public char RequiredCharacter { get; }
+
+        /// <summary>
+        /// Either the minimum number of times the character may
+        /// appear in the password, or one of the indexes that
+        /// the character may appear in, depending on the policy.
+        /// </summary>
         public int Min { get; }
+
+        /// <summary>
+        /// Either the maximum number of times the character may
+        /// appear in the password, or one of the indexes that
+        /// the character may appear in, depending on the policy.
+        /// </summary>
         public int Max { get; }
 
+        /// <summary>
+        /// Creates an instance of <see cref="PasswordPolicy"/>.
+        /// </summary>
+        /// <param name="requiredCharacter">The required character.</param>
+        /// <param name="min">The minimum number.</param>
+        /// <param name="max">The maximum number.</param>
         public PasswordPolicy(char requiredCharacter, int min, int max)
         {
             RequiredCharacter = requiredCharacter;
@@ -16,14 +42,42 @@ namespace AdventOfCode2020
             Max = max;
         }
 
+        /// <summary>
+        /// Checks that the given password contains the required character
+        /// at least <see cref="Min"/> times and at most <see cref="Max"/>
+        /// times (inclusive).
+        /// </summary>
+        /// <param name="password">The password to check</param>
+        /// <returns>
+        /// Whether the given password contains the required character
+        /// at least <see cref="Min"/> times and at most <see cref="Max"/>
+        /// times (inclusive).
+        /// </returns>
         public bool OldSystemCheck(string password)
         {
+            if (password is null)
+                throw new ArgumentNullException(nameof(password));
+
             var count = password.Count(c => c == RequiredCharacter);
             return count >= Min && count <= Max;
         }
 
+        /// <summary>
+        /// Checks that the given password contains the required character
+        /// at either index <see cref="Min"/> or index <see cref="Max"/>
+        /// (where 1 is the first position), but not both.
+        /// </summary>
+        /// <param name="password">The password to check</param>
+        /// <returns>
+        /// Whether the given password contains the required character
+        /// at either index <see cref="Min"/> or index <see cref="Max"/>
+        /// (where 1 is the first position), but not both.
+        /// </returns>
         public bool NewSystemCheck(string password)
         {
+            if (password is null)
+                throw new ArgumentNullException(nameof(password));
+
             return (password[Min - 1] == RequiredCharacter) ^ (password[Max - 1] == RequiredCharacter);
         }
     }
