f74df8686fb36b22559277d9965267b362456ca0
= <josh@hufford.io>
2020-12-03 17:03:35 -0500

    Add doc comments and argument checks

--- AdventOfCode2020/Classes/TobogganMap.cs
+++ AdventOfCode2020/Classes/TobogganMap.cs
@@ -4,14 +4,25 @@ using System.Linq;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// Represents an individual tile on a <see cref="TobogganMap"/>.
+    /// </summary>
     public enum TobogganMapTile
     {
         Empty,
         Tree,
     }
 
+    /// <summary>
+    /// Represents a map of trees. The map repeats to the right forever.
+    /// </summary>
     public class TobogganMap
     {
+        /// <summary>
+        /// A mapping from characters used in the text representation of the
+        /// map to a <see cref="TobogganMapTile"/>.
+        /// </summary>
+        /// <value></value>
         public static readonly Dictionary<char, TobogganMapTile> Characters = new Dictionary<char, TobogganMapTile>
         {
             ['.'] = TobogganMapTile.Empty,
@@ -20,40 +31,85 @@ namespace AdventOfCode2020
 
         private TobogganMapTile[,] map;
 
+        /// <summary>
+        /// Returns the <see cref="TobogganMapTile"/> located a cordinate
+        /// (<paramref name="x"/>, <paramref name="y"/>). This accounts for the
+        /// map looping forever to the right.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="TobogganMapTile"/> located a cordinate
+        /// (<paramref name="x"/>, <paramref name="y"/>).
+        /// </returns>
         public TobogganMapTile this[int x, int y] => this.map[x % this.map.GetLength(0), y];
 
+        /// <summary>
+        /// Creates an instance of <see cref="TobogganMap"/>.
+        /// </summary>
+        /// <param name="map">A grid of <see cref="TobogganMapTile"/> objects.</param>
         public TobogganMap(TobogganMapTile[,] map)
         {
             this.map = map;
         }
 
+        /// <summary>
+        /// Returns an array of tiles encountered by starting at (0,0) and
+        /// travelling <paramref name="dx"/> tile right and
+        /// <paramref name="dy"/> tiles down until reaching the bottom of the
+        /// map.
+        /// </summary>
+        /// <param name="dx">The number of tiles to move right each step.</param>
+        /// <param name="dy">The number of tiles to move right each step.</param>
+        /// <returns></returns>
         public TobogganMapTile[] GetPath(int dx, int dy)
         {
+            if (dx < 0)
+                throw new ArgumentException(nameof(dx), $"{nameof(dx)} must be non-negative.");
+            if (dy <= 0)
+                throw new ArgumentException(nameof(dx), $"{nameof(dx)} must be positive.");
+
             return Enumerable.Range(0, this.map.GetLength(1) / dy)
                 .Select(i => this[i * dx, i * dy])
                 .ToArray();
         }
 
+        /// <summary>
+        /// Creates an instance of <see cref="TobogganMap"/> from a text
+        /// representation. The characters used in the map are mapped to
+        /// <see cref="TobogganMapTile"/> objects using
+        /// <see cref="Characters"/>.
+        /// </summary>
+        /// <param name="input">A text representation of the map.</param>
+        /// <returns>A new <see cref="TobogganMap"/> based on the input.</returns>
         public static TobogganMap Parse(string input)
         {
+            if (input is null)
+                throw new ArgumentNullException(nameof(input));
+
+            if (input is "")
+                throw new ArgumentException(nameof(input), "Input must not be empty.");
+
             var lines = input.Split('\n');
             var widths = lines.Select(x => x.Length);
             if (widths.Distinct().Count() != 1)
-                throw new ArgumentException(nameof(input));
+                throw new ArgumentException(nameof(input), "All lines must be the same length.");
             var width = widths.First();
+
+            if (width == 0)
+                throw new ArgumentException(nameof(input), "Map width must be greater than 0.");
 
             TobogganMapTile[,] trees = new TobogganMapTile[width, lines.Length];
             for (int y = 0; y < lines.Length; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    if (Characters.TryGetValue(lines[y][x], out TobogganMapTile tile))
+                    var c = lines[y][x];
+                    if (Characters.TryGetValue(c, out TobogganMapTile tile))
                     {
                         trees[x, y] = tile;
                     }
                     else
                     {
-                        throw new ArgumentException(nameof(input));
+                        throw new ArgumentException(nameof(input), $"Unexpected character '{c}' at line {y} position {x}.");
                     }
                 }
             }
