58cc40436c004d2337dcb62553e8482ad44e222d
= <josh@hufford.io>
2020-12-04 17:35:54 -0500

    Setup for day 4

---
+++ AdventOfCode2020Tests/Day04Tests.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+using AdventOfCode2020;
+using System.Linq;
+
+namespace AdventOfCode2020Tests
+{
+    public class Day04Tests
+    {
+        [Fact]
+        public void Part1Test()
+        {
+            // Given
+
+            // When
+
+            // Then
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Part2Test(int dx, int dy, int expectedTrees)
+        {
+            // Given
+
+            // When
+
+            // Then
+            Assert.True(false); // TODO:
+        }
+    }
+}
