b70e36ac69b1877f98b71dd22a56c4d28309f83c
= <josh@hufford.io>
2020-12-05 12:47:32 -0500

    Write test for day 5 part 1

--- AdventOfCode2020Tests/Day05Tests.cs
+++ AdventOfCode2020Tests/Day05Tests.cs
@@ -7,15 +7,19 @@ namespace AdventOfCode2020Tests
 {
     public class Day05Tests
     {
-        [Fact]
-        public void Part1Test()
+        [Theory]
+        [InlineData("BFFFBBFRRR", 70, 7, 567)]
+        [InlineData("FFFBBBFRRR", 14, 7, 119)]
+        [InlineData("BBFFBBFRLL", 102, 4, 820)]
+        public void Part1Test(string specification, int row, int column, int seatId)
         {
             // Given
-
-            // When
+            var boardingPass = new BoardingPass(specification);
 
             // Then
-            Assert.True(false);
+            Assert.Equal(row, boardingPass.Row);
+            Assert.Equal(column, boardingPass.Column);
+            Assert.Equal(seatId, boardingPass.SeatId);
         }
     }
 }
