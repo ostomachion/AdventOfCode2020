c4e055161f68fc2024efecc0f8c015dbf2bbb1d8
= <josh@hufford.io>
2020-12-05 13:19:45 -0500

    Add doc comments

--- AdventOfCode2020/Classes/BoardingPass.cs
+++ AdventOfCode2020/Classes/BoardingPass.cs
@@ -3,13 +3,38 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// Represents a boarding pass containing seating information.
+    /// </summary>
+    /// <remarks>Used with <see cref="Day05"/>.</remarks>
     public class BoardingPass
     {
+        /// <summary>
+        /// The string on the boarding pass that encodes the seating
+        /// information using "binary space partitioning".
+        /// </summary>
         public string Specification { get; }
+
+        /// <summary>
+        /// The row number, from 0 to 127.
+        /// </summary>
         public int Row { get; }
+
+        /// <summary>
+        /// The column number, from 0 to 7.
+        /// </summary>
         public int Column { get; }
+
+        /// <summary>
+        /// The seat id, determined uniquely by the row and column numbers.
+        /// </summary>
         public int SeatId => Row * 8 + Column;
 
+        /// <summary>
+        /// Creates an instance of <see cref="BoaringPass"/> from a boarding
+        /// pass specification string.
+        /// </summary>
+        /// <param name="specification">The specificatoin string to parse.</param>
         public BoardingPass(string specification)
         {
             Specification = specification ?? throw new ArgumentNullException(nameof(specification));
