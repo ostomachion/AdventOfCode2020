9cff932b012f809c3a9573731d5fe4e23565fd2a
= <josh@hufford.io>
2020-12-05 13:10:15 -0500

    Implement BoardingPass

--- AdventOfCode2020/Classes/BoardingPass.cs
+++ AdventOfCode2020/Classes/BoardingPass.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020
 {
@@ -7,11 +8,16 @@ namespace AdventOfCode2020
         public string Specification { get; }
         public int Row { get; }
         public int Column { get; }
-        public int SeatId => throw new NotImplementedException();
+        public int SeatId => Row * 8 + Column;
 
         public BoardingPass(string specification)
         {
-            throw new NotImplementedException();
+            Specification = specification ?? throw new ArgumentNullException(nameof(specification));
+            if (!Regex.IsMatch(specification, @"^[BF]{7}[LR]{3}$"))
+                throw new FormatException("Invalid specification.");
+
+            Row = Convert.ToInt32(specification.Substring(0, 7).Replace("F", "0").Replace("B", "1"), 2);
+            Column = Convert.ToInt32(specification.Substring(7, 3).Replace("L", "0").Replace("R", "1"), 2);
         }
 
         public BoardingPass(int row, int column)
