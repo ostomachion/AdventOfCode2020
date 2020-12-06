c3bd048c2432a624e5177e3ec0d9820f0b2e4d9c
= <josh@hufford.io>
2020-12-06 14:56:32 -0500

    Add doc comments

--- AdventOfCode2020/Classes/CustomsDeclarationFormAnswers.cs
+++ AdventOfCode2020/Classes/CustomsDeclarationFormAnswers.cs
@@ -1,19 +1,49 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// Represents an individual's answers to a customs declaration form.
+    /// </summary>
     public class CustomsDeclarationFormAnswers
     {
+        /// <summary>
+        /// A list of questions on the customs declaration form.
+        /// </summary>
         public static readonly IEnumerable<char> Questions = "abcdefghijklmnopqrstuvwxyz";
 
         private readonly HashSet<char> yesAnswers = new HashSet<char>();
 
+        /// <summary>
+        /// Creates an instance of <see cref="CustomsDeclarationFormAnswers"/>
+        /// from a string.
+        /// </summary>
+        /// <param name="text">
+        /// The string to parse. Each character represents a question that the
+        /// individual answered yes to.
+        /// </param>
         public CustomsDeclarationFormAnswers(string text)
         {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+
             yesAnswers = text.ToCharArray().ToHashSet();
+
+            if (yesAnswers.Except(Questions).Any())
+                throw new FormatException("Invalid format.");
         }
 
-        public bool Answer(char c) => yesAnswers.Contains(c);
+        /// <summary>
+        /// Determines whether the individual answered yes to the given
+        /// question.
+        /// </summary>
+        /// <param name="question">The question to check.</param>
+        /// <returns>
+        /// <c>true</c> if the individual answered yes to the given question;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public bool Answer(char question) => yesAnswers.Contains(question);
     }
 }
--- AdventOfCode2020/Classes/CustomsDeclarationFormGroupAnswers.cs
+++ AdventOfCode2020/Classes/CustomsDeclarationFormGroupAnswers.cs
@@ -1,14 +1,33 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020
 {
+    /// <summary>
+    /// Represents a group's answers to custom declaration forms.
+    /// </summary>
     public class CustomsDeclarationFormGroupAnswers
     {
+        /// <summary>
+        /// A list of each <see cref="CustomDeclarationFormAnswers"/> for each
+        /// member of the group.
+        /// </summary>
         public IEnumerable<CustomsDeclarationFormAnswers> Answers { get; }
 
+        /// <summary>
+        /// Creates an instance of
+        /// <see cref="CustomsDeclarationFormGroupAnswers"/> from a string.
+        /// </summary>
+        /// <param name="text">
+        /// The string to parse. Each line of the string should be a valid
+        /// <see cref="CustomDeclarationsFormAnswers"/> string.
+        /// </param>
         public CustomsDeclarationFormGroupAnswers(string text)
         {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+
             Answers = text.Split("\n").Select(x => new CustomsDeclarationFormAnswers(x));
         }
     }
