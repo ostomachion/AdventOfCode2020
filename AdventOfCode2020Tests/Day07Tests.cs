afb1820e4b0d2cd3b3c4dfd36a8a98525cc26bb9
= <josh@hufford.io>
2020-12-07 19:57:55 -0500

    Setup for day 7

---
+++ AdventOfCode2020Tests/Day07Tests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using AdventOfCode2020;
+using System.Linq;
+
+namespace AdventOfCode2020Tests
+{
+    public class Day07Tests
+    {
+        [Fact]
+        public void Part1Test()
+        {
+            // Given
+
+            // When
+
+            // Then
+            Assert.True(false);
+        }
+    }
+}
