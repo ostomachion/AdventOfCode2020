b5a2b8b73ad377abb9528b2a85680a951fc408cc
= <josh@hufford.io>
2020-12-09 18:13:06 -0500

    Setup for day 9

---
+++ AdventOfCode2020/Day09.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace AdventOfCode2020
+{
+    public static class Day09
+    {
+        /// <summary>
+        /// https://adventofcode.com/2020/day/9
+        /// </summary>
+        public static void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/9#part2
+        /// </summary>
+        public static void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2020Tests/Day09Tests.cs
@@ -0,0 +1,59 @@
+using System;
+using Xunit;
+using AdventOfCode2020;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020Tests
+{
+    public class Day09Tests
+    {
+        [Fact]
+        public void Part1Test()
+        {
+            // Given
+
+            // When
+
+            // Then
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Part2Test()
+        {
+            // Given
+            var program = HandheldProgram.Parse(Input.Get(8));
+
+            for (var i = 0; i < program.Instructions.Count(); i++)
+            {
+                // When
+                var modified = Day08.ModifyProgram(program, i);
+                if (modified is null)
+                    continue;
+                var computer = new HandheldComputer(modified);
+                var accumulator = computer.Accumulator;
+
+                var visited = new HashSet<int>();
+
+                while (!computer.IsHalted && !visited.Contains(computer.ProgramCounter))
+                {
+                    accumulator = computer.Accumulator;
+                    visited.Add(computer.ProgramCounter);
+                    computer.Step();
+                }
+
+                // Then
+                if (i == 7)
+                {
+                    Assert.True(computer.IsHalted);
+                    Assert.Equal(8, accumulator);
+                }
+                else
+                {
+                    Assert.False(computer.IsHalted);
+                }
+            }
+        }
+    }
+}
