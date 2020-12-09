21de92384924e9a0758c1c739cc61f8e99b18b09
= <josh@hufford.io>
2020-12-08 19:22:33 -0500

    Setup for day 8

---
+++ AdventOfCode2020/Day08.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace AdventOfCode2020
+{
+    public static class Day08
+    {
+        /// <summary>
+        /// https://adventofcode.com/2020/day/8
+        /// </summary>
+        public static void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/8#part2
+        /// </summary>
+        public static void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
