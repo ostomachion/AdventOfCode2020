837e09a5736bbf878cc658987bb2a3d9e614d53f
= <josh@hufford.io>
2020-12-10 18:41:33 -0500

    Setup for day 10

---
+++ AdventOfCode2020/Day10.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace AdventOfCode2020
+{
+    public static class Day10
+    {
+        /// <summary>
+        /// https://adventofcode.com/2020/day/10
+        /// </summary>
+        public static void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/10#part2
+        /// </summary>
+        public static void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
