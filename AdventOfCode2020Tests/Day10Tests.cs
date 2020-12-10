8f57781aa9a57bbbb0c4ffc33dc5ce5c64addc63
= <josh@hufford.io>
2020-12-10 18:58:21 -0500

    Add first test for day 10 part 1

--- AdventOfCode2020Tests/Day10Tests.cs
+++ AdventOfCode2020Tests/Day10Tests.cs
@@ -13,11 +13,20 @@ namespace AdventOfCode2020Tests
         public void Part1Test()
         {
             // Given
+            var joltages = Input.GetLines(10, "Short")
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Prepend(0)
+                .ToList();
+            joltages.Add(joltages.Max() + 3);
 
             // When
+            var differences = Enumerable.Range(0, joltages.Count - 1)
+                .Select(i => joltages[i + 1] - joltages[i]);
 
             // Then
-            Assert.True(false);
+            Assert.Equal(7, differences.Count(x => x == 1));
+            Assert.Equal(5, differences.Count(x => x == 3));
         }
     }
 }
