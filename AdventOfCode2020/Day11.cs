f3e1226f15db65badf11af1e486eb815e4665b1e
= <josh@hufford.io>
2020-12-11 18:14:07 -0500

    Setup for day 11

---
+++ AdventOfCode2020/Day11.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace AdventOfCode2020
+{
+    public static class Day11
+    {
+        /// <summary>
+        /// https://adventofcode.com/2020/day/11
+        /// </summary>
+        public static void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/11#part2
+        /// </summary>
+        public static void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
