9e239d81c83e208feda670a83a8865a370bf1296
= <josh@hufford.io>
2020-12-12 17:34:14 -0500

    Add missing file

---
+++ AdventOfCode2020/Classes/Ship.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace AdventOfCode2020
+{
+    public class Ship
+    {
+        public int Orientation { get; }
+        public int X { get; }
+        public int Y { get; }
+
+        public void FollowRoute(Route route)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(RouteInstruction instruction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
