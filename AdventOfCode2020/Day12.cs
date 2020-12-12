4810aba2f3f802affa6f88877e76cedef6152791
= <josh@hufford.io>
2020-12-12 17:24:42 -0500

    Setup for day 12 part 1

---
+++ AdventOfCode2020/Day12.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace AdventOfCode2020
+{
+    public static class Day12
+    {
+        /// <summary>
+        /// https://adventofcode.com/2020/day/12
+        /// </summary>
+        public static void Part1()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// https://adventofcode.com/2020/day/12#part2
+        /// </summary>
+        public static void Part2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
 using AdventOfCode2020;
 
-Console.WriteLine("Day 11 Part 2");
-Day11.Part2();
+Console.WriteLine("Day 12 Part 1");
+Day12.Part1();
